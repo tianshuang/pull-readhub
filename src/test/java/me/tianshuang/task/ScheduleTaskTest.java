4cdc8e83820e4e9fa0e9ab60e5e7dc6b86b5babd
Poison <tianshuang.me@gmail.com>
2017-11-03 10:13:20 +0800

    optimize

--- src/test/java/me/tianshuang/task/ScheduleTaskTest.java
+++ src/test/java/me/tianshuang/task/ScheduleTaskTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:pull-readhubme.properties")
 public class ScheduleTaskTest {
 
     @Autowired
